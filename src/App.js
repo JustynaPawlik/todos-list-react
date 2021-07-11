49c1850daaf2381fb02f087b64dd2d974dce6a36
Justyna Pawlik <juspaw11@wp.pl>
2021-07-11 11:38:57 +0200

    added StyledNavLink

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import { HashRouter, NavLink, Switch, Route, Redirect } from "react-router-dom";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import React from "react";
 import AuthorPage from "./features/author/AuthorPage";
 import {TaskPage} from './features/tasks/TaskPage';
 import TasksPage from './features/tasks/TasksPage/index';
+import { StyledNavLink } from "./styled";
 
 export default () => (
     <HashRouter>
     <nav>
         <ul>
             <li>
-                <NavLink activeClassName="active" to="/zadania">Zadania</NavLink>
+                <StyledNavLink to="/zadania">Zadania</StyledNavLink>
             </li>
             <li>
-                <NavLink activeClassName="active" to="/autor">O autorze</NavLink>
+                <StyledNavLink to="/autor">O autorze</StyledNavLink>
             </li>
         </ul>
             <Switch>
