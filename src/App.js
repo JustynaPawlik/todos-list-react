611783b02064ebcc3e1e1e24a2b7d2d765e18694
Justyna Pawlik <juspaw11@wp.pl>
2021-07-13 07:51:38 +0200

    removed StyledNavLinks from App.js

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ import { Navigation } from "./Navigation";
 export default () => (
     <HashRouter>
     <Navigation />
-        <ul>
-            <li>
-                <StyledNavLink to="/zadania">Zadania</StyledNavLink>
-            </li>
-            <li>
-                <StyledNavLink to="/autor">O autorze</StyledNavLink>
-            </li>
-        </ul>
             <Switch>
                 <Route path="/zadania/:id">
                     <TaskPage />
